bfb27e9db5fb6a6e3e444661cb10d3089ed11a5c
Atakan Yaşar <atakany357@gmail.com>
2024-10-26 15:00:33 +0300

    add trained model

--- Week 3/ML-agents-sample/Assets/Scripts/AgentScript.cs
+++ Week 3/ML-agents-sample/Assets/Scripts/AgentScript.cs
@@ -11,6 +11,7 @@ public class AgentScript : Agent
     public GameObject target;
     
     private Rigidbody agentRb;
+    private float previousDistance = float.MaxValue;
 
     public override void Initialize()
     {
@@ -19,24 +20,33 @@ public class AgentScript : Agent
 
     public override void OnEpisodeBegin()
     {
-        // Reset the target position
-        target.transform.position = new Vector3(Random.Range(-4.5f, 4.5f), 0.5f, Random.Range(-4.5f, 4.5f));
+        // Reset the target local position
+        target.transform.localPosition = new Vector3(Random.Range(-4.5f, 4.5f), 0.5f, Random.Range(-4.5f, 4.5f));
+        target.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        target.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
         
         // Reset the agent position
-        transform.position = new Vector3(Random.Range(-4.5f, 4.5f), 0.5f, Random.Range(-4.5f, 4.5f));
+        transform.localPosition = new Vector3(Random.Range(-4.5f, 4.5f), 0.5f, Random.Range(-4.5f, 4.5f));
+        transform.localRotation = Quaternion.Euler(new Vector3(0, Random.Range(0, 360)));
+
         agentRb.velocity = Vector3.zero;
         agentRb.angularVelocity = Vector3.zero;
+
+        previousDistance = Vector3.Distance(this.transform.localPosition, target.transform.localPosition);
+
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
         // Target and Agent positions
-        sensor.AddObservation(target.transform.position);
-        sensor.AddObservation(this.transform.position);
+        sensor.AddObservation(target.transform.localPosition);
+        sensor.AddObservation(this.transform.localPosition);
+        sensor.AddObservation(this.transform.localRotation);
         
         // Agent velocity
         sensor.AddObservation(agentRb.velocity.x);
         sensor.AddObservation(agentRb.velocity.z);
+        sensor.AddObservation(agentRb.angularVelocity);
     }
 
     public void OnCollisionEnter(Collision collision)
@@ -60,6 +70,14 @@ public class AgentScript : Agent
         controlSignal.x = actions.ContinuousActions[0];
         controlSignal.z = actions.ContinuousActions[1];
         agentRb.AddForce(controlSignal * 10);
+
+        // Rewards
+        float distanceToTarget = Vector3.Distance(this.transform.localPosition, target.transform.localPosition);
+        AddReward((previousDistance - distanceToTarget) * 0.01f);
+        // Debug.Log("Reward: " + (previousDistance - distanceToTarget) * 0.01f);
+        previousDistance = distanceToTarget;
+        
+
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
